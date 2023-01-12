bfab4749639e8d9ad046168670dbdb1e3c0b9c65
Yash Gupta <ygupta2001@gmail.com>
2023-01-12 16:59:45 +0530

    Created Note java class and changed the themes.xml

---
+++ app/src/main/java/com/yashgupta/notesapp/Note.java
@@ -0,0 +1,31 @@
+package com.yashgupta.notesapp;
+
+public class Note extends RealmObject {
+    String title;
+    String description;
+    long timestamp;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
---
+++ app/src/main/java/com/yashgupta/notesapp/RealmObject.java
@@ -0,0 +1,4 @@
+package com.yashgupta.notesapp;
+
+public class RealmObject {
+}
